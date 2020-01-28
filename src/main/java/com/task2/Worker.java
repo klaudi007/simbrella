b80d3cb42569314532641ecd39384a4ccf2ff5f5
Musa <klaudi2012@gmail.com>
2020-01-28 13:46:03 +0400

    Modified and simplified second task

--- src/main/java/com/task2/Worker.java
+++ src/main/java/com/task2/Worker.java
@@ -7,7 +7,6 @@ import java.io.FileWriter;
 import java.util.Stack;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author MusaAl
@@ -19,39 +18,49 @@ public class Worker {
     private static final int THREAD_COUNT = 5;
 
     public static void main(String[] args) {
+
         System.out.println("Start");
 
-        StringBuilder sb = new StringBuilder();
+        StringBuffer sb = new StringBuffer();
 
         try (BufferedReader br = new BufferedReader(new FileReader("stackable_items.txt"));
              BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"))) {
+            /**
+             *   Pushing data to Stack
+             */
             br.lines().forEach(stack::push);
 
 
             ExecutorService executor = Executors.newFixedThreadPool(THREAD_COUNT);
 
+            /**
+             *   Reading data from Stack
+             */
             Runnable r = () -> {
                 if (!stack.empty()) {
 
-                    for(int i = 0; i<6; i++){
-                        if(stack.isEmpty()) break;
+                    for (int i = 1; i < 6; i++) {
+
+                        if (stack.isEmpty()) break;
                         String str = stack.pop();
-                        String line = String.format("%s\t%s\t%s\t%s", Thread.currentThread().getName(), System.currentTimeMillis(), str, str.substring(4));
-                        sb.append(line).append(System.lineSeparator());
-                        System.out.println("line -> "+line);
+                        String line = String.format("%s\t%s\t%s\t%s\n", Thread.currentThread().getName(), System.currentTimeMillis(), str, i);
+                        sb.append(line);
+                        System.out.println("line -> " + line);
+
                     }
 
                 }
             };
 
 
-            int size = stack.size();
-            while(size-- > 0){
+            while (!stack.isEmpty()) {
                 executor.execute(r);
             }
 
 
-
+            /**
+             *   Read data to output file
+             */
             while (true) {
                 if (stack.empty()) {
                     System.out.println("stack is empty");
